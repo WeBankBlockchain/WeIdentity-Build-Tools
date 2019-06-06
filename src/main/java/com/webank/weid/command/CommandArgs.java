ca78fc4c3c3b86f8812598bd72403a5855b18731
tonychen <tonychen@webank.com>
2019-06-06 16:37:03 +0800

    * modify build-tools script and command line code

--- src/main/java/com/webank/weid/command/CommandArgs.java
+++ src/main/java/com/webank/weid/command/CommandArgs.java
@@ -4,49 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.beust.jcommander.Parameter;
-
 import lombok.Getter;
 
 /**
  * command line parameter parser.
- * @author tonychen 2019年6月4日
  *
+ * @author tonychen 2019/6/4
  */
 @Getter
 public class CommandArgs {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    @Parameter
+    private List<String> parameters = new ArrayList<>();
 
-	@Parameter
-	private List<String> parameters = new ArrayList<>();
+    @Parameter(names = {"--weid"}, description = "weidentity did")
+    private String weid;
 
-	@Parameter(names = { "--weid" }, description = "weidentity did")
-	private String weid;
+    @Parameter(names = "--cpt-dir", description = "the directory which the cpt json files will be found and be registered to blockchain.")
+    private String cptDir;
 
-	@Parameter(names = "--cpt-dir", description = "the directory which the cpt json files will be found and be registered to blockchain.")
-	private String cptDir;
+    @Parameter(names = "--private-key", description = "private key file")
+    private String privateKey;
 
-	@Parameter(names = "--private-key", description = "private key file")
-	private String privateKey;
-	
-	@Parameter(names = "--cpt-list", description = "cpt id list")
-	private String cptIdList;
-	
-	@Parameter(names = "--cpt-id", description = "cpt id")
-	private String cptId;
-	
-	@Parameter(names = "--cpt-file", description = "cpt file")
-	private String cptFile;
-	
-	@Parameter(names = "--org-name", description = "org name")
-	private String orgName;
+    @Parameter(names = "--cpt-list", description = "cpt id list")
+    private String cptIdList;
 
-	@Parameter(names = "--type", description = "type")
-	private String type;
+    @Parameter(names = "--cpt-id", description = "cpt id")
+    private String cptId;
+
+    @Parameter(names = "--cpt-file", description = "cpt file")
+    private String cptFile;
+
+    @Parameter(names = "--org-id", description = "org name")
+    private String orgId;
+
+    @Parameter(names = "--type", description = "type")
+    private String type;
 }
--- src/main/java/com/webank/weid/command/RegistCpt.java
+++ src/main/java/com/webank/weid/command/RegistCpt.java
@@ -22,13 +22,13 @@ package com.webank.weid.command;
 import java.io.File;
 import java.io.IOException;
 
+import com.beust.jcommander.JCommander;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.beust.jcommander.JCommander;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.github.fge.jackson.JsonLoader;
 import com.webank.weid.constant.ErrorCode;
 import com.webank.weid.constant.FileOperator;
 import com.webank.weid.protocol.base.CptBaseInfo;
@@ -42,106 +42,110 @@ import com.webank.weid.util.DataToolUtils;
 import com.webank.weid.util.FileUtils;
 
 /**
- * @author tonychen 2019年4月11日
- *
+ * @author tonychen 2019/4/11
  */
 public class RegistCpt {
 
-	 /**
+    /**
      * log4j.
      */
     private static final Logger logger = LoggerFactory.getLogger(RegistCpt.class);
-	
-	private static CptService cptService = new CptServiceImpl();
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
 
-		if (args == null || args.length < 6) {
-			logger.error("[RegisterCpt] input parameters error, please check your input!");
-			System.exit(1);
-		}
+    private static CptService cptService = new CptServiceImpl();
 
-		CommandArgs commandArgs = new CommandArgs();
-    	JCommander.newBuilder()
-    	  .addObject(commandArgs)
-    	  .build()
-    	  .parse(args);
-    	
-		String weid = commandArgs.getWeid();
-		String cptDir = commandArgs.getCptDir();
-		String privateKeyFile = commandArgs.getPrivateKey();
-		String cptId = commandArgs.getCptId();
-		String cptFile = commandArgs.getCptFile();
-		
-		WeIdAuthentication weIdAuthentication = new WeIdAuthentication();
-		weIdAuthentication.setWeId(weid);
-		String privateKey = FileUtils.readFile(privateKeyFile);
-		WeIdPrivateKey weIdPrivateKey = new WeIdPrivateKey();
-		weIdPrivateKey.setPrivateKey(privateKey);
-		weIdAuthentication.setWeIdPrivateKey(weIdPrivateKey);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		if(StringUtils.isNotEmpty(cptFile)) {
-			File cptFile1 = new File(cptFile);
-			registerCpt(cptFile1, cptId, weIdAuthentication);
-		}
-		
-		if(StringUtils.isNotEmpty(cptDir)) {
-			File file = new File(cptDir);
+        if (args == null || args.length < 6) {
+            logger.error("[RegisterCpt] input parameters error, please check your input!");
+            System.exit(1);
+        }
 
-			if (!file.isDirectory()) {
-				logger.error("failed.");
-				System.exit(1);
-			}
-			for (File f : file.listFiles()) {
-				registerCpt(f,cptId, weIdAuthentication);
-			}
-		}
-		
-		System.exit(0);
-	}
+        CommandArgs commandArgs = new CommandArgs();
+        JCommander.newBuilder()
+            .addObject(commandArgs)
+            .build()
+            .parse(args);
 
-	private static void registerCpt(File cptFile, String cptId, WeIdAuthentication weIdAuthentication) {
-		JsonNode jsonNode;
-		try {
-			String fileName = cptFile.getName();
-			if (!fileName.endsWith(".json")) {
-				return;
-			}
-			System.out.println("[registerCpt] begin to register cpt file:"+fileName);
-			jsonNode = JsonLoader.fromFile(cptFile);
-			String cptJsonSchema = jsonNode.toString();
-			CptStringArgs cptStringArgs = new CptStringArgs();
-			cptStringArgs.setCptJsonSchema(cptJsonSchema);
-			cptStringArgs.setWeIdAuthentication(weIdAuthentication);
-			
-			ResponseData<CptBaseInfo> response;
-			if(StringUtils.isEmpty(cptId)) {
-				response = cptService.registerCpt(cptStringArgs);
-			}else {
-				Integer cptId1 = Integer.valueOf(cptId);
-				response = cptService.registerCpt(cptStringArgs, cptId1);
-			}
-			System.out.println("[RegisterCpt] result:" + DataToolUtils.serialize(response));
-			if (!response.getErrorCode().equals(ErrorCode.SUCCESS.getCode())) {
-				logger.error("[RegisterCpt] load config faild. ErrorCode is:{}, msg :{}", response.getErrorCode(),
-						response.getErrorMessage());
-				System.out.println("[RegisterCpt] register cpt file:"+fileName+" failed, errorcode is "+ response.getErrorCode());
-			}
-			String content = new StringBuffer()
-					.append(fileName)
-					.append("=")
-					.append(String.valueOf(response.getResult().getCptId()))
-					.append("\r\n")
-					.toString();
-			FileUtils.writeToFile(content, "regist_cpt.out", FileOperator.APPEND);
-			System.out.println("[RegisterCpt] register cpt file:"+fileName+" successfully, errorcode is \"");
-		} catch (IOException e) {
-			logger.error("[RegisterCpt] load config faild. ", e);
-			System.exit(1);
-		}
-	}
-	
+        String weid = commandArgs.getWeid();
+        String cptDir = commandArgs.getCptDir();
+        String privateKeyFile = commandArgs.getPrivateKey();
+        String cptId = commandArgs.getCptId();
+        String cptFile = commandArgs.getCptFile();
+
+        WeIdAuthentication weIdAuthentication = new WeIdAuthentication();
+        weIdAuthentication.setWeId(weid);
+        String privateKey = FileUtils.readFile(privateKeyFile);
+        WeIdPrivateKey weIdPrivateKey = new WeIdPrivateKey();
+        weIdPrivateKey.setPrivateKey(privateKey);
+        weIdAuthentication.setWeIdPrivateKey(weIdPrivateKey);
+
+        if (StringUtils.isNotEmpty(cptFile)) {
+            File cptFile1 = new File(cptFile);
+            registerCpt(cptFile1, cptId, weIdAuthentication);
+        }
+
+        if (StringUtils.isNotEmpty(cptDir)) {
+            File file = new File(cptDir);
+
+            if (!file.isDirectory()) {
+                logger.error("failed.");
+                System.exit(1);
+            }
+            for (File f : file.listFiles()) {
+                registerCpt(f, cptId, weIdAuthentication);
+            }
+        }
+
+        System.exit(0);
+    }
+
+    private static void registerCpt(File cptFile, String cptId,
+        WeIdAuthentication weIdAuthentication) {
+        JsonNode jsonNode;
+        try {
+            String fileName = cptFile.getName();
+            if (!fileName.endsWith(".json")) {
+                return;
+            }
+            System.out.println("[registerCpt] begin to register cpt file:" + fileName);
+            jsonNode = JsonLoader.fromFile(cptFile);
+            String cptJsonSchema = jsonNode.toString();
+            CptStringArgs cptStringArgs = new CptStringArgs();
+            cptStringArgs.setCptJsonSchema(cptJsonSchema);
+            cptStringArgs.setWeIdAuthentication(weIdAuthentication);
+
+            ResponseData<CptBaseInfo> response;
+            if (StringUtils.isEmpty(cptId)) {
+                response = cptService.registerCpt(cptStringArgs);
+            } else {
+                Integer cptId1 = Integer.valueOf(cptId);
+                response = cptService.registerCpt(cptStringArgs, cptId1);
+            }
+            System.out.println("[RegisterCpt] result:" + DataToolUtils.serialize(response));
+            if (!response.getErrorCode().equals(ErrorCode.SUCCESS.getCode())) {
+                logger.error("[RegisterCpt] load config faild. ErrorCode is:{}, msg :{}",
+                    response.getErrorCode(),
+                    response.getErrorMessage());
+                System.out.println(
+                    "[RegisterCpt] register cpt file:" + fileName + " failed, errorcode is "
+                        + response.getErrorCode());
+            }
+            String content = new StringBuffer()
+                .append(fileName)
+                .append("=")
+                .append(String.valueOf(response.getResult().getCptId()))
+                .append("\r\n")
+                .toString();
+            FileUtils.writeToFile(content, "regist_cpt.out", FileOperator.APPEND);
+            System.out.println(
+                "[RegisterCpt] register cpt file:" + fileName + " successfully, errorcode is \"");
+        } catch (IOException e) {
+            logger.error("[RegisterCpt] load config faild. ", e);
+            System.exit(1);
+        }
+    }
+
 }

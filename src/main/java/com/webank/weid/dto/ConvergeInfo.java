51ccb390253ce07a3c5de067324777a697e0e99c
yg36305356 <125480926@qq.com>
2020-04-06 14:55:40 +0800

    * add code for offline

---
+++ src/main/java/com/webank/weid/dto/ConvergeInfo.java
@@ -0,0 +1,22 @@
+package com.webank.weid.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ConvergeInfo {
+    // 汇聚编号
+    private String convergeId;
+    // 文件路径
+    private String filePath;
+    //数据来源Ip
+    private String formIp;
+    // 文件时间
+    private String dataTime;
+    // 汇聚状态，0初始状态，1失败，2成功
+    private int status;
+    
+    private Date createTime;
+    private Date updateTime;
+}
---
+++ src/main/java/com/webank/weid/service/DataBaseService.java
@@ -0,0 +1,85 @@
+package com.webank.weid.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.webank.weid.constant.DataDriverConstant;
+import com.webank.weid.constant.SqlConstant;
+import com.webank.weid.suite.api.persistence.Persistence;
+import com.webank.weid.suite.persistence.sql.ConnectionPool;
+import com.webank.weid.suite.persistence.sql.SqlExecutor;
+import com.webank.weid.suite.persistence.sql.driver.MysqlDriver;
+
+@Service
+public class DataBaseService {
+    /**
+     * log4j.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(DataBaseService.class);
+    
+    // sql执行器
+    private static SqlExecutor sqlExecutor;
+    
+    private static final String CHECK_TABLE_SQL =
+        "SELECT table_name "
+        + DataDriverConstant.SQL_COLUMN_DATA
+        + " FROM information_schema.TABLES "
+        + " WHERE upper(table_name) = upper('$1')"
+        + " and upper(table_schema) = upper('$2')";
+    
+    private static Persistence persistence;
+    
+    private Persistence getPersistence() {
+        if (persistence == null) {
+            persistence = new MysqlDriver();
+        }
+        return persistence;
+    }
+    
+    private SqlExecutor getSqlExecutor() {
+        if (sqlExecutor == null) {
+            sqlExecutor = new SqlExecutor(ConnectionPool.getFirstDataSourceName()); 
+        }
+        return sqlExecutor;
+    }
+    
+    /**
+     * 初始化表操作.
+     */
+    public void initDataBase() {
+        initConfig();
+        initTable();
+    }
+    
+    //初始化系统配置
+    private void initConfig() {
+        //初始化Domain, 预留未来默认初始化系统配置
+        getPersistence();
+        logger.info("[DataBaseService.initConfig] init successfully.");
+    }
+    //初始化自定义表
+    private void initTable() {
+        //初始化异步记录表
+        initTable(SqlConstant.TABLE_NAME_ASYNC_INFO, SqlConstant.DDL_TABLE_NAME_ASYNC_INFO);
+        initTable(
+            SqlConstant.TABLE_NAME_OFFLINE_TRANSACTION_INFO, 
+            SqlConstant.DDL_TABLE_NAME_OFFLINE_TRANSACTION_INFO
+        );
+    }
+    
+    private void initTable(String tableName, String createTableSql) {
+        String checkTableSql = buildCheckSql(CHECK_TABLE_SQL, tableName);
+        boolean result = getSqlExecutor().createTable(checkTableSql, createTableSql);
+        if (result) {
+            logger.info("[DataBaseService.initTable] {} init successfully.", tableName);
+        } else {
+            logger.error("[DataBaseService.initTable] {} init fail.", tableName);
+        }
+    }
+    
+    private String buildCheckSql(String exeSql, String tableName) {
+        exeSql = exeSql.replace(SqlExecutor.TABLE_CHAR, tableName);
+        return exeSql;
+    }
+}

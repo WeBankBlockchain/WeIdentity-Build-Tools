a107830e3d7019654b147b828578b7bd0ae63dbb
yg36305356 <125480926@qq.com>
2020-04-06 14:55:40 +0800

    * add code for offline

---
+++ src/main/java/com/webank/weid/dto/BinLog.java
@@ -0,0 +1,37 @@
+package com.webank.weid.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class BinLog extends BaseDto {
+    
+    private String id;
+    // 交易请求Id
+    @JsonProperty("request_id")
+    private String requestId;
+    // 交易方法
+    @JsonProperty("transaction_method")
+    private String transactionMethod;
+    // 交易参数
+    @JsonProperty("transaction_args")
+    private String transactionArgs;
+    // 扩展 
+    private String extra;
+    // 交易时间
+    @JsonProperty("transaction_timestamp")
+    private long transactionTimestamp;
+    // 文件时间
+    private int batch;
+    // 交易状态
+    private int status;
+    // 创建时间
+    private Date created;
+    // 更新时间
+    private Date updated;
+}

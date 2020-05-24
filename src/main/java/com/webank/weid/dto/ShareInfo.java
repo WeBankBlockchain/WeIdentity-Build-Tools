7a6122ee1af187e043bcaa7418d94951e5fe98f7
yg3630536 <125480926@qq.com>
2020-05-24 14:03:00 +0800

    * add code for deploy the evidence by groupId

---
+++ src/main/java/com/webank/weid/dto/ShareInfo.java
@@ -0,0 +1,26 @@
+package com.webank.weid.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ShareInfo extends DeployBase {
+
+    private String owner;
+    private String ownerShow;
+    private Integer groupId;
+    private String showGroupId;
+    private String evidenceAddress;
+    private boolean enable; 
+    
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+        this.showGroupId = "group-" + groupId;
+    }
+    
+    public void setOwner(String owner) {
+        this.owner = owner;
+        this.ownerShow = super.getHideValue(owner, 20, 6);
+    }
+}

e6a9976d2ba7ae2eaeeabdbb7b129cdace26309b
yg3630536 <125480926@qq.com>
2020-06-01 12:01:22 +0800

    * add tool for migrate the evidence address

---
+++ tools/register_evidence.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ..
+source ./script/common.inc
+
+cd ${SOURCE_CODE_DIR}
+
+if [ $# -lt 4 ] ;then
+    echo "input error. please check your input."
+    exit 1
+fi
+
+build_classpath
+
+echo "begin register evidence address into cns, please wait..."
+
+java ${JAVA_OPTS} -cp "$CLASSPATH" com.webank.weid.command.RegisterEvidenceByGroup $@
+
+if [ ! $? -eq 0 ]; then
+    echo "register faild, please check the log -> ../logs/error.log."
+    exit $?;
+fi

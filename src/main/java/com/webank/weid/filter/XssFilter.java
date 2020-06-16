ebf875fc7bc1e8980caabafbbd587a47cc584239
yg3630536 <125480926@qq.com>
2020-06-16 10:51:15 +0800

    * upgrade jar and add xss filter

---
+++ src/main/java/com/webank/weid/filter/XssFilter.java
@@ -0,0 +1,37 @@
+package com.webank.weid.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = "/*")
+public class XssFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(
+        ServletRequest servletRequest, 
+        ServletResponse servletResponse, 
+        FilterChain filterChain
+    ) throws IOException, ServletException {
+        filterChain.doFilter(
+            new XssHttpServletRequestWraper((HttpServletRequest) servletRequest),
+            servletResponse
+        );
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

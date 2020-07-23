830246574a61f1def83670599c79a31cb43cd4f6
yg3630536 <125480926@qq.com>
2020-07-23 17:54:23 +0800

    * fixed comments

--- src/main/java/com/webank/weid/controller/IndexController.java
+++ src/main/java/com/webank/weid/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.webank.weid.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.webank.weid.service.DeployService;
 
 /**
  * 主页控制器.
@@ -10,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/")
 public class IndexController {
     
+    @Autowired
+    DeployService deployService;
+    
     @RequestMapping(value = "/")
     public String index(){
-        return "redirect:index.html";
+       String value = deployService.getGuideStatus(); 
+        if (StringUtils.isBlank(value)) {
+            return "redirect:guide.html";
+        } else {
+            return "redirect:index.html";
+        }
     }
 }

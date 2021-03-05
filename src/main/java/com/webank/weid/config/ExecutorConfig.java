31e95995b5d54f1f45a988b6025d6db8f93b0a0d
yg36305356 <125480926@qq.com>
2021-03-05 17:56:45 +0800

    add new web code

--- src/main/java/com/webank/weid/config/ExecutorConfig.java
+++ src/main/java/com/webank/weid/config/ExecutorConfig.java
@@ -28,9 +28,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import lombok.Data;
+
 @Configuration
 @EnableAsync
 @ConfigurationProperties(prefix = "async.thread")
+@Data
 public class ExecutorConfig {
 
     private int corePoolSize;

068eb8278c90dcfde273e6478121bf001d843108
youwenbusi <1245549353@qq.com>
2023-06-15 12:35:51 +0800

    upgrade shells foe deploying

--- src/main/java/com/webank/weid/dto/DeployBase.java
+++ src/main/java/com/webank/weid/dto/DeployBase.java
@@ -10,8 +10,8 @@ import lombok.EqualsAndHashCode;
 public class DeployBase extends BaseDto {
     private String nodeVerion;
     private String nodeAddress;
-    private String ecdsaKey;
-    private String ecdsaPublicKey;
+    private String privateKey;
+    private String publicKey;
     private String contractVersion;
     private String weIdSdkVersion;
     private boolean local;
